c1c8954737cebdc9bb4444b0d59a3db1933992a4
AkorpuzZ <akorpuz1987@gmail.com>
2024-01-09 14:37:30 -0700

    primer version plantilla en blanco

---
+++ src/main/java/net/akrop/gaiaclasses/GaiaClasses.java
@@ -0,0 +1,39 @@
+package net.akrop.gaiaclasses;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod(GaiaClasses.MOD_ID)
+public class GaiaClasses
+{
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "gaiaclasses";
+
+    public GaiaClasses()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
